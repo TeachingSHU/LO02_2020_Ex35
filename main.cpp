f5e2f8e2a21157147618e6811e33289ed959d4fc
Paul Blondel <paulb@ixian.ai>
2020-11-03 17:31:20 +0800

    init: init commit add(cmake): add CMakeList.txt add(source): add source files add(codeblocks): add project file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "container.h"
+
+using namespace std;
+
+int main()
+{
+
+    // IMPOSSIBLE !! Car Container est virtuel pure !
+    //Container ctr;
+
+    return 0;
+}
